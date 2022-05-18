4f92893d31d915b35b420e3365c3ad8a57cf7753
jbranaman <96451672+jbranaman@users.noreply.github.com>
2022-05-18 11:55:58 -0400

    Add files via upload

---
+++ world_happiness_tables_15-19.sql
@@ -0,0 +1,55 @@
+CREATE TABLE world_happiness_2015 (
+     Region CHAR(40) NOT NULL,
+     Happiness_Rank INT NOT NULL,
+	 Happiness_Score NUMERIC NOT NULL,
+	 Economy_GDP_per_Capita NUMERIC NOT NULL,
+	 Family NUMERIC NOT NULL,
+	 Health_Life_Expectancy NUMERIC NOT NULL,
+	 Freedom NUMERIC NOT NULL,
+	 Trust_Government_Corruption NUMERIC NOT NULL,
+	 Generosity NUMERIC NOT NULL
+);
+CREATE TABLE world_happiness_2016 (
+     Region CHAR(40) NOT NULL,
+     Happiness_Rank INT NOT NULL,
+	 Happiness_Score NUMERIC NOT NULL,
+	 Economy_GDP_per_Capita NUMERIC NOT NULL,
+	 Family NUMERIC NOT NULL,
+	 Health_Life_Expectancy NUMERIC NOT NULL,
+	 Freedom NUMERIC NOT NULL,
+	 Trust_Government_Corruption NUMERIC NOT NULL,
+	 Generosity NUMERIC NOT NULL
+);
+CREATE TABLE world_happiness_2017 (
+     Country CHAR(40) NOT NULL,
+     Happiness_Rank INT NOT NULL,
+	 Happiness_Score NUMERIC NOT NULL,
+	 Economy_GDP_per_Capita NUMERIC NOT NULL,
+	 Family NUMERIC NOT NULL,
+	 Health_Life_Expectancy NUMERIC NOT NULL,
+	 Freedom NUMERIC NOT NULL,
+	 Trust_Government_Corruption NUMERIC NOT NULL,
+	 Generosity NUMERIC NOT NULL
+);
+CREATE TABLE world_happiness_2018 (
+     Country CHAR(40) NOT NULL,
+     Happiness_Rank INT NOT NULL,
+	 Happiness_Score NUMERIC NOT NULL,
+	 Economy_GDP_per_Capita NUMERIC NOT NULL,
+	 Family NUMERIC NOT NULL,
+	 Health_Life_Expectancy NUMERIC NOT NULL,
+	 Freedom NUMERIC NOT NULL,
+	 Trust_Government_Corruption NUMERIC NOT NULL,
+	 Generosity NUMERIC NOT NULL
+);
+CREATE TABLE world_happiness_2019 (
+     Country CHAR(40) NOT NULL,
+     Happiness_Rank INT NOT NULL,
+	 Happiness_Score NUMERIC NOT NULL,
+	 Economy_GDP_per_Capita NUMERIC NOT NULL,
+	 Family NUMERIC NOT NULL,
+	 Health_Life_Expectancy NUMERIC NOT NULL,
+	 Freedom NUMERIC NOT NULL,
+	 Trust_Government_Corruption NUMERIC NOT NULL,
+	 Generosity NUMERIC NOT NULL
+);
